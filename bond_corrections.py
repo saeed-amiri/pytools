2421a159eed75c685cac816ccd710cd4d76f2793
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-05-13 09:56:31 +0200

    Update bond_corrections.py
    read coeffs from header ata

--- bond_corrections.py
+++ bond_corrections.py
@@ -1,5 +1,5 @@
 import os, sys, re
-import readline
+from pprint import pprint
 import pandas as pd
 import numpy as np
 
@@ -9,6 +9,28 @@ class DOC:
     These scripts read the data file from write_data, check the distance between particles that have a bond between them;
     if the distance between two particles that have shared a bond is bigger than the HALF OF THE BOX SIZE, move the particle,
     with smaller z close to the other one, i.e., just adding the length of the box to the z component.
+    The script is wrote for DATAFILE which have hybrid style, i.e.:
+    
+    Pair Coeffs # lj/cut/coul/long
+
+    1 lj/cut/coul/long 0.1553 3.166
+    2 lj/cut/coul/long 0 0
+    
+    Bond Coeffs # harmonic
+
+    1 harmonic 600 1
+    2 harmonic 268 1.526
+    
+    Angle Coeffs # harmonic
+
+    1 harmonic 75 109.47
+    2 harmonic 58.35 112.7
+
+    Dihedral Coeffs # opls
+
+    1 opls 1.3 -0.05 0.2 0
+    2 opls 0 0 0.3 0
+    
     usages: {sys.argv[0]} system.data
     """
 
@@ -24,25 +46,96 @@ class HEADER:
     """
     def __init__(self) -> None:
         self.atomsLine = 0
+        self.atomsLine = self.check_file()
+        print(f'number of header lines: {self.atomsLine}\n')
+        self.read_header()
+        pprint(self.PairCoeff)
 
-    def check_file(self) -> None:
+
+    def check_file(self) -> int:
         FILECHECK = False
-        MAXHEADER = 200
+        MAXHEADER = 1000
         linecount = 0
         with open(DATAFILE, 'r') as f:
             while True:
                 linecount += 1
                 line = f.readline()
-                print(line.strip())
                 if line.startswith('Atoms'):
                     FILECHECK = True
-                    self.atomsLine = linecount
+                    atomsLine = linecount
                     break
                 if linecount > MAXHEADER:
                     err = FILEERROR()
                     exit(err.__doc__)
                 if not line:
                     exit("wrong data file\n")
+        return atomsLine
+
+    def read_header(self):
+        """read header to get all the available info"""
+        self.Masses, self.PairCoeff, self.BondCoeff, self.AngleCoeff, self.DihedralCoeff=dict(),dict(),dict(),dict(),dict()
+        Masses, PairCoeff, BondCoeff, AngleCoeff, DihedralCoeff=False, False, False, False, False
+        linecount = 0
+        with open(DATAFILE, 'r') as f:
+            while True:
+                linecount += 1
+                line = f.readline()
+                if line.strip().endswith("atoms"):      self.NATOMS = int(line.strip().split(' ')[0])
+                if line.strip().endswith("atom types"): self.NATomTyp = int(line.strip().split(' ')[0])
+                if line.strip().endswith("bonds"):      self.NBonds = int(line.strip().split(' ')[0])
+                if line.strip().endswith("bond types"): self.NBondTyp = int(line.strip().split(' ')[0])
+                if line.strip().endswith("angles"):     self.NAngles = int(line.strip().split(' ')[0])
+                if line.strip().endswith("angle types"):self.NAngleTyp = int(line.strip().split(' ')[0])
+                if line.strip().endswith("dihedrals"):  self.NDihedrals = int(line.strip().split(' ')[0])
+                if line.strip().endswith("dihedral typss"): self.NDihedralsTyp = int(line.strip().split(' ')[0])
+                if line.strip().endswith("xhi"): self.Xlim = self.get_axis_lim(line.strip().split('xlo')[0])
+                if line.strip().endswith("yhi"): self.Ylim = self.get_axis_lim(line.strip().split('ylo')[0])
+                if line.strip().endswith("zhi"): self.Zlim = self.get_axis_lim(line.strip().split('zlo')[0])
+
+                if line.strip().startswith("Masses"):
+                    Masses, PairCoeff, BondCoeff, AngleCoeff, DihedralCoeff = True, False, False, False, False
+                if line.strip().startswith("Pair"):
+                    Masses, PairCoeff, BondCoeff, AngleCoeff, DihedralCoeff = False, True, False, False, False
+                if line.strip().startswith("Bond"):
+                    Masses, PairCoeff, BondCoeff, AngleCoeff, DihedralCoeff = False, False, True, False, False
+                if line.strip().startswith("Angle"):
+                    Masses, PairCoeff, BondCoeff, AngleCoeff, DihedralCoeff = False, False, False, True, False
+                if line.strip().startswith("Dihedral"):
+                    Masses, PairCoeff, BondCoeff, AngleCoeff, DihedralCoeff = False, False, False, False, True
+                if line.strip():
+                    if Masses: self.get_masses(line.strip(), 'Masses')
+                    if PairCoeff: self.get_pair_coeff(line.strip(), 'Pair')
+                    # if Masses: self.get_masses(line.strip(), 'Masses')
+                    # if Masses: self.get_masses(line.strip(), 'Masses')
+                # if linecount > self.atomsLine and not Masses: 
+                    # print(linecount, Masses)
+                    # err = FILEERROR()
+                    # exit(f'{err.__doc__}')
+                if not line: break
+
+    def get_axis_lim(self, lim):
+        lim = lim.split(' ')
+        lim = [item for item in lim if item]
+        return lim
+    
+    def get_masses(self, line, check):
+        if check not in line: 
+            typ = line.split(' ')[0]
+            mass = float(line.split(' ')[1])
+            self.Masses[typ]=mass
+        else: pass
+
+    def get_pair_coeff(self, line, check):
+        if check not in line: 
+            line = line.split(' ')
+            typ = line[0]
+            self.PairCoeff[typ]=dict(style=line[1], coeff=line[2:])
+        else: pass
+
+
+
+
+        
 
 class ATOMS:
     """
@@ -82,4 +175,4 @@ if __name__ == "__main__":
         exit(f'\nONE INPUT IS RWUIRED\n{doc.__doc__}')
     DATAFILE = sys.argv[1]
     header = HEADER()
-    header.check_file()
+    print(header.__dict__)
