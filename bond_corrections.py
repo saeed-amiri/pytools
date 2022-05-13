b99f2723d9e2670670973f5f096b5869664c43ef
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-05-13 07:45:24 +0200

    add classes

--- bond_corrections.py
+++ bond_corrections.py
@@ -6,6 +6,50 @@ class DOC:
     """
     A script to correct the bond problem in LAMMPS's write_data command and the boundary conditions.
     These scripts read the data file from write_data, check the distance between particles that have a bond between them;
-    if the distance between two particles that have shared a bond is bigger than the HALF OF THE BOX SIZE, move the particle,\
-        with smaller z close to the other one, i.e., just adding the length of the box to the z component.
+    if the distance between two particles that have shared a bond is bigger than the HALF OF THE BOX SIZE, move the particle,
+    with smaller z close to the other one, i.e., just adding the length of the box to the z component.
+    usages: {sys.argv[0]} system.data
     """
+
+class HEADER:
+    """
+    read haeder data of the data file
+    """
+    def __init__(self) -> None:
+        pass
+
+class ATOMS:
+    """
+    read atoms coordinates
+    """
+    def __init__(self) -> None:
+        pass
+
+class BONDS:
+    """
+    read bonds
+    """
+    def __init__(self) -> None:
+        pass
+
+class ANGLES:
+    """
+    read angles
+    """
+    def __init__(self) -> None:
+        pass
+
+class DIHEDRALS:
+    """
+    read dihedrals
+    """
+    def __init__(self) -> None:
+        pass
+
+
+
+# check the input file 
+if len(sys.argv)==1:
+    doc = DOC()
+    exit(f'\nONE INPUT IS RWUIRED\n{doc.__doc__}')
+
