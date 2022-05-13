c38552e8a37586e782ce4f0c5e7ad95e2d218b0d
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-05-13 08:07:05 +0200

    check the input file

--- bond_corrections.py
+++ bond_corrections.py
@@ -1,4 +1,5 @@
 import os, sys, re
+import readline
 import pandas as pd
 import numpy as np
 
@@ -11,12 +12,37 @@ class DOC:
     usages: {sys.argv[0]} system.data
     """
 
+class FILEERROR:
+    """
+    there is problem in the header of the DATAFILE,
+    maybe long header!\n
+    """
+
 class HEADER:
     """
     read haeder data of the data file
     """
     def __init__(self) -> None:
-        pass
+        self.atomsLine = 0
+
+    def check_file(self) -> None:
+        FILECHECK = False
+        MAXHEADER = 200
+        linecount = 0
+        with open(DATAFILE, 'r') as f:
+            while True:
+                linecount += 1
+                line = f.readline()
+                print(line.strip())
+                if line.startswith('Atoms'):
+                    FILECHECK = True
+                    self.atomsLine = linecount
+                    break
+                if linecount > MAXHEADER:
+                    err = FILEERROR()
+                    exit(err.__doc__)
+                if not line:
+                    exit("wrong data file\n")
 
 class ATOMS:
     """
@@ -48,8 +74,12 @@ class DIHEDRALS:
 
 
 
-# check the input file 
-if len(sys.argv)==1:
-    doc = DOC()
-    exit(f'\nONE INPUT IS RWUIRED\n{doc.__doc__}')
 
+if __name__ == "__main__":
+    # check the input file 
+    if len(sys.argv)==1:
+        doc = DOC()
+        exit(f'\nONE INPUT IS RWUIRED\n{doc.__doc__}')
+    DATAFILE = sys.argv[1]
+    header = HEADER()
+    header.check_file()
