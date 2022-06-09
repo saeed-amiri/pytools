84412330d6aad1931cfa264bfc28bddd3428a7b3
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-06-09 16:42:52 +0200

    Converting LAMMPS data file to PDB
      This scripts temp to convert LAMMPS full atom style file to PDB file
        which can easily be read by VMD, ...

---
+++ lmp_to_pdb.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import re
+
+class Doc:
+    """ Converting LAMMPS data file to PDB 
+    This scripts temp to convert LAMMPS full atom style file to PDB file
+    which can easily be read by VMD, ...
+    """
+
