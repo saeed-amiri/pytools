d578b932ff4bc738271c68fd331b0bf04211667a
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-07-27 09:55:51 +0200

    Update read_mutli_run_log.py

--- read_mutli_run_log.py
+++ read_mutli_run_log.py
@@ -1,11 +1,12 @@
 import os
+from pprint import pprint
 import sys
 import pandas as pd
 
 
 class Doc:
     """read log.lammps and extract thermo_style output
-    This script reads the log without time_reset 0 and appends all
+    This script reads the log without 'time_reset 0' and appends all
     the thermo after time_reset to a DataFrame.
     The thermo_style must include 'Step' key, the rest is arbitrary
     and also during the simulation the thermo_style should reamin the
@@ -25,6 +26,12 @@ class ReadLog:
 
     def read_log(self) -> None:
         """reading the file"""
+        lines: list[str]  # A list of the thermo lines
+        header: str  # A str of the thermo_style
+        lines, header = self.get_lines()
+        self.df = self.mk_df(lines, header)
+
+    def get_lines(self) -> tuple[list[str], str]:
         run: bool = False  # If line starts with 'run'
         loop: bool = False  # If line starts with 'Loop'
         step: bool = False  # If line starts with 'Step'
@@ -35,17 +42,34 @@ class ReadLog:
                 line = f.readline()
                 if line.strip().startswith('run'):
                     run = True
-                if line.strip().startswith('Step'):
+                elif line.strip().startswith('Step'):
                     step = True
-                    head = line
-                if line:
-                    if run and step:
-                        line_list.append(line)
-                if line.strip().startswith('Loop'):
+                    head = line.strip()
+                elif line.strip().startswith('Loop'):
                     step = False
                     run = False
+                elif line and not loop:
+                    if run and step:
+                        line_list.append(line.strip())
                 if not line:
                     break
+        return line_list, head
+
+    def mk_df(self, lines: list[str], header: str) -> pd.DataFrame:
+        """make DataFrame from the list of the lines"""
+        columns_names: list[str] = self.break_header(header)
+        line_list: list[list[str]] = self.break_lines(lines)
+        df = pd.DataFrame(line_list, columns=columns_names)
+        df.drop_duplicates(subset=['Step'], inplace=True)
+        return df
+
+    def break_header(self, header: str) -> list[str]:
+        return header.split(' ')
+
+    def break_lines(self, lines: list[str]) -> list[list[str]]:
+        """break the lines of the thermo-lines"""
+        temp = [item.split(' ') for item in lines]
+        return [[sub_item for sub_item in item if sub_item] for item in temp]
 
 
 if __name__ == "__main__":
